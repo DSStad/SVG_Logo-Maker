311cd9d51d5003ccf5a66b4515708d00e3a53e0b
Derick Stadler <derickstadler@gmail.com>
2023-06-14 17:49:04 -0600

    updated index file with inquirer prompt of questions

--- index.js
+++ index.js
@@ -1,2 +1,32 @@
-const { Circle }= require('./lib/shapes');
-const fs = require('fs');
+const { Shape, Circle, Square, Triangle } = require("./lib/shapes");
+const fs = require("fs");
+const inquirer = require("inquirer");
+
+buildSVG = () => {
+  return inquirer.prompt([
+    {
+      type: "input",
+      message:
+        "What text would you like in your logo(must be 3 characters or less)?",
+      name: "text",
+    },
+    {
+      type: "input",
+      message: "What color would you like for text to be in the logo?",
+      name: "textFill",
+    },
+    {
+      type: "list",
+      name: "shape",
+      message: "Please choose a shape for your logo?",
+      choices: ["Circle", "Triangle", "Square"],
+      default: "Circle",
+    },
+    {
+      type: "input",
+      message:
+        "What color would you like the shape to be filled with in the logo?",
+      name: "fill",
+    },
+  ]);
+};
