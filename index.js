4959a330d8638bba78b30c9151bc65ac38c66bd3
Derick Stadler <derickstadler@gmail.com>
2023-06-14 22:07:05 -0600

    added more logic to index.js to handle shapes question and created new shapePicker file to potentially handle picking

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-// const { Shape, Circle, Square, Triangle } = require("./lib/shapes");
-// const fs = require("fs");
+const { Shape, Circle, Square, Triangle } = require("./lib/shapes");
+const fs = require("fs");
 const inquirer = require("inquirer");
+const { squareCorners } = require("cli-spinners");
 
 // const buildSVG = () => {
 inquirer
@@ -32,7 +33,26 @@ inquirer
   .then((answers) => {
     const newLogo = answers;
     console.log(newLogo);
+    const shape = newLogo.shape;
+    console.log(shape, typeof shape);
+    if (shape === "Triangle") {
+      const triLogo = new Triangle(
+        newLogo.fill,
+        newLogo.text,
+        newLogo.textFill
+      );
+      return triLogo;
+    } else if (shape === "Square") {
+      const sqrLogo = new Square(newLogo.fill, newLogo.text, newLogo.textFill);
+      return sqrLogo;
+    } else if (shape === "Circle") {
+      const circLogo = new Circle(newLogo.fill, newLogo.text, newLogo.textFill);
+      return circLogo;
+    }
   });
+//   .then(() => {
+//       fs.writeFile("./output/logo.svg", )
+//   })
 // };
 
 // buildSVG();
