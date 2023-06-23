fd86145d17dc807f3e6544224d1866b5082535b1
Derick Stadler <derickstadler@gmail.com>
2023-06-22 19:39:30 -0600

    fixed testing issue and ran successful tests

--- tests/shapes.test.js
+++ tests/shapes.test.js
@@ -7,12 +7,8 @@ describe("Shape", () => {
       const text = "BBB";
       const textFill = "white";
       const circle = new Shapes.Circle(fill, text, textFill);
-      expect(
-        circle
-          .render()
-          .toEqual(
-            `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg"><circle cx="150" cy="100" r="80" fill="red" /><text x="150" y="125" font-size="60" text-anchor="middle" fill="white">BBB</text></svg>`
-          )
+      expect(circle.render()).toEqual(
+        `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000 svg"><circle cx="150" cy="100" r="80" fill="${fill}" /><text x="150" y="125" font-size="60" text-anchor="middle" fill="${textFill}">${text}</text></svg>`
       );
     });
   });
@@ -23,12 +19,8 @@ describe("Shape", () => {
       const text = "BBB";
       const textFill = "white";
       const square = new Shapes.Square(fill, text, textFill);
-      expect(
-        square
-          .render()
-          .toEqual(
-            `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg"><rect x="70" y="20" width="160" height="160" fill="red" /><text x="150" y="125" font-size="60" text-anchor="middle" fill="white">BBB</text></svg>`
-          )
+      expect(square.render()).toEqual(
+        `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg"><rect x="70" y="20" width="160" height="160" fill="${fill}" /><text x="150" y="125" font-size="60" text-anchor="middle" fill="${textFill}">${text}</text></svg>`
       );
     });
   });
@@ -39,10 +31,8 @@ describe("Shape", () => {
       const text = "BBB";
       const textFill = "white";
       const triangle = new Shapes.Triangle(fill, text, textFill);
-      expect(
-        triangle.render()
-          .toEqual(`<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg"><polygon points="150, 10 250, 140 50, 140" fill="red"/><text x="150" y="125" font-size="60" text-anchor="middle" fill="white">BBB</text>
-                </svg>`)
+      expect(triangle.render()).toEqual(
+        `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg"><polygon points="150, 10 250, 140 50, 140" fill="${fill}"/><text x="150" y="125" font-size="60" text-anchor="middle" fill="${textFill}">${text}</text></svg>`
       );
     });
   });
